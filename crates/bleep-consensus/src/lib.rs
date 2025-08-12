1cd7c9e3363b2339fc49172e88f869335cde6087
Muhammad Attahir <88341523+TaahirSaif@users.noreply.github.com>
2025-08-12 10:55:55 +0100

    Update lib.rs
    Signed-off-by: Muhammad Attahir <88341523+TaahirSaif@users.noreply.github.com>

--- crates/bleep-consensus/src/lib.rs
+++ crates/bleep-consensus/src/lib.rs
@@ -1,3 +1,9 @@
-pub mod consensus;
 pub mod ai_adaptive_logic;
+pub mod blockchain_state;
+pub mod consensus;
+pub mod networking;
 pub mod tests;
+
+pub use consensus::{BLEEPAdaptiveConsensus, ConsensusMode, Validator};
+pub use blockchain_state::BlockchainState;
+pub use networking::NetworkingModule;
