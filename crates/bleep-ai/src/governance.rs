876cff700461486f26f8e0acf6c23f226ff00144
Haleemzy20 <halimaabdulazeez654@gmail.com>
2025-08-18 12:42:21 +0100

    Add files via upload
    Signed-off-by: Haleemzy20 <halimaabdulazeez654@gmail.com>

---
+++ crates/bleep-ai/src/governance.rs
@@ -0,0 +1,4 @@
+pub struct BLEEPGovernance;
+impl BLEEPGovernance {
+	pub async fn get_active_proposals_ref(_this: &Self) -> Result<(), ()> { Ok(()) }
+}
---
+++ crates/bleep-ai/src/interoperability.rs
@@ -0,0 +1,4 @@
+pub struct InteroperabilityModule;
+impl InteroperabilityModule {
+	pub async fn get_status_ref(_this: &Self) -> Result<(), ()> { Ok(()) }
+}
