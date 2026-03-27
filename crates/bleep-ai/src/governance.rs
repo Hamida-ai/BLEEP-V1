2c88ffb96427f60572dd9922adc76f26eb7c5db8
Hamida-ai <muhammadhamida860@gmail.com>
2026-03-27 10:33:33 +0100

    Update governance.rs
    Signed-off-by: Hamida-ai <muhammadhamida860@gmail.com>

--- crates/bleep-ai/src/governance.rs
+++ crates/bleep-ai/src/governance.rs
@@ -1,4 +1,6 @@
+#[allow(dead_code)]
 pub struct SelfAmendingGovernance;
+#[allow(dead_code)]
 impl SelfAmendingGovernance {
 	pub fn new() -> Self { Self }
 	pub fn submit_proposal(&self, _name: String, _creator: String) -> Result<u64, String> { Ok(1) }
@@ -6,7 +8,9 @@ impl SelfAmendingGovernance {
 	pub fn is_approved(&self, _proposal_id: u64) -> Result<bool, String> { Ok(true) }
 	pub fn execute_proposal(&self, _proposal_id: u64) -> Result<bool, String> { Ok(true) }
 }
+#[allow(dead_code)]
 pub struct BLEEPGovernance;
+#[allow(dead_code)]
 impl BLEEPGovernance {
 	pub async fn get_active_proposals_ref(_this: &Self) -> Result<(), ()> { Ok(()) }
 }
